d8d11b14802c6af10b761117d737f22353a5323d
parkseon-yeong <nab07111@naver.com>
2023-04-09 18:42:49 +0900

    Initial commit

---
+++ src/main/java/aitrainer/aitrainerspring/AitrainerSpringApplication.java
@@ -0,0 +1,13 @@
+package aitrainer.aitrainerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitrainerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitrainerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aitrainer/aitrainerspring/AitrainerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package aitrainer.aitrainerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitrainerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
